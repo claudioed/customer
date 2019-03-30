cfbcc62aa4d99c422ddd4b23f760a153081c443f
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2019-03-30 15:49:56 -0300

    customers implementation

--- src/main/kotlin/tech/claudioed/customer/MainVerticle.kt
+++ src/main/kotlin/tech/claudioed/customer/MainVerticle.kt
@@ -2,25 +2,80 @@ package tech.claudioed.customer
 
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
+import io.vertx.core.json.JsonObject
+import io.vertx.core.logging.LoggerFactory
+import io.vertx.ext.mongo.MongoClient
+import io.vertx.grpc.VertxServerBuilder
+import tech.claudioed.customer.grpc.CustomerCreateRequest
+import tech.claudioed.customer.grpc.CustomerFindRequest
+import tech.claudioed.customer.grpc.CustomerFindResponse
+import tech.claudioed.customer.grpc.CustomerServiceGrpc
+import java.util.*
 
 class MainVerticle : AbstractVerticle() {
 
+  private val LOGGER = LoggerFactory.getLogger(MainVerticle::class.java)
+
   override fun start(startFuture: Future<Void>) {
-    vertx
-      .createHttpServer()
-      .requestHandler { req ->
-        req.response()
-          .putHeader("content-type", "text/plain")
-          .end("Hello from Vert.x!")
-      }
-      .listen(8888) { http ->
-        if (http.succeeded()) {
-          startFuture.complete()
-          println("HTTP server started on port 8888")
-        } else {
-          startFuture.fail(http.cause());
+
+    val mongoHost = System.getenv("MONGO_HOST")
+    LOGGER.info(" MONGO HOST $mongoHost")
+    val mongoClient = MongoClient.createShared(this.vertx, JsonObject()
+      .put("connection_string", mongoHost)
+      .put("db_name", "CUSTOMERS"))
+
+
+    val service = object : CustomerServiceGrpc.CustomerServiceImplBase() {
+      override fun findCustomer(request: CustomerFindRequest,
+                                responseObserver: io.grpc.stub.StreamObserver<CustomerFindResponse>) {
+        val query = JsonObject().put("_id", request.id)
+        mongoClient.findOne("customers", query, null) {
+          if (it.succeeded()) {
+            LOGGER.info("Query executed successfully")
+            val json = it.result()
+            LOGGER.info("Converting Data")
+            val customerData = CustomerFindResponse.newBuilder().setId(json.getString("_id")).setName(json.getString("_name"))
+              .setAddress(json.getString("address")).setDocument(json.getString("document"))
+              .setCountry(json.getString("country")).setCity(json.getString("city"))
+              .setEmail(json.getString("email")).setLastName(json.getString("lastName"))
+              .build()
+            responseObserver.onNext(customerData)
+            responseObserver.onCompleted()
+          } else {
+            responseObserver.onError(RuntimeException("Customer Not Found"))
+          }
         }
       }
+
+      override fun createCustomer(request: CustomerCreateRequest,
+                                  responseObserver: io.grpc.stub.StreamObserver<CustomerFindResponse>) {
+
+        val customer = Customer(id = UUID.randomUUID().toString(), name = request.name, lastName = request.lastName, city = request.city, country = request.country, address = request.address, document = request.document, email = request.email)
+        val data = JsonObject().put("id",customer.id).put("name",customer.name).put("lastName",customer.lastName).put("city",customer.city).put("country",customer.country)
+          .put("address",customer.address).put("document",customer.document).put("email",customer.email)
+        mongoClient.insert("customers",data) {
+          if(it.succeeded()){
+            LOGGER.info("Customer saved successfully")
+            val response = CustomerFindResponse.newBuilder().setId(customer.id).setName(customer.name)
+              .setAddress(customer.address).setDocument(customer.document)
+              .setCountry(customer.country).setCity(customer.city)
+              .setEmail(customer.email).setLastName(customer.lastName)
+              .build()
+            responseObserver.onNext(response)
+            responseObserver.onCompleted()
+          }else{
+            responseObserver.onError(RuntimeException("Error to insert customer"))
+          }
+        }
+      }
+    }
+
+    val rpcServer = VertxServerBuilder
+      .forAddress(vertx, "localhost", 8080)
+      .addService(service)
+      .build()
+
+    rpcServer.start()
 
   }
 
